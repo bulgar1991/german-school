aaf947c0761c3686cb2601b819053f7ee08ceb20
Igor <igor.b@gbl-factory.org>
2026-04-01 23:27:35 +0300

    task-2: Added the banner.

---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import {TranslateModule} from "@ngx-translate/core";
+
+@Component({
+  selector: 'app-banner',
+  imports: [TranslateModule],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.scss',
+})
+export class BannerComponent {}
