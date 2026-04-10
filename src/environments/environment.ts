751e2fd246b81f7e929cae5ba3cdf175a03abaa9
Igor <igor.b@gbl-factory.org>
2026-04-10 13:28:25 +0300

    form-application: Updated the form application.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // Get your free key at https://web3forms.com
-  web3formsKey: 'YOUR_WEB3FORMS_KEY',
-};
+  web3formsKey: '0045ecb4-90a8-4d09-90be-ba8095a002eb',
+}
